2adf8877ee77aebe0c4aea8f9fe5f34d2f899847
Vlsarro <drekmaro@gmail.com>
2016-08-02 12:31:44 +0300

    fixed bugs with url mapping and enabled proper name view for wine objects

--- recom_source/recom_source/urls.py
+++ recom_source/recom_source/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 
 # Later it should be divided in sub-categories, weapon, wine, etc.
 urlpatterns = [
-    url(r'^reviews/', include('reviews.rls', namespace="reviews")),
+    url(r'^reviews/', include('reviews.urls', namespace="reviews")),
     url(r'^admin/', admin.site.urls),
 ]
