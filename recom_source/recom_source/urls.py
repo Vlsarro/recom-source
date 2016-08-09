1bc80ccb5ab80cef26857b23d9f8b4a2ba715539
Vlsarro <drekmaro@gmail.com>
2016-08-09 11:12:23 +0300

    Added registration

--- recom_source/recom_source/urls.py
+++ recom_source/recom_source/urls.py
@@ -6,5 +6,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^reviews/', include('reviews.urls', namespace="reviews")),
     url(r'^admin/', admin.site.urls),
-    url('^accounts/', include('django.contrib.auth.urls', namespace="auth"))
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls', namespace="auth"))
 ]
